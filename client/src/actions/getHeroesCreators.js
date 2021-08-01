7484e2c648d77019f4588328c5239064758b2c60
EvgenStr <juve2910@gmail.com>
2021-08-02 01:19:09 +0300

    add get heroes creators

---
+++ client/src/actions/getHeroesCreators.js
@@ -0,0 +1,16 @@
+import ACTION_TYPES from './actionTypes';
+
+
+export const getHeroesRequest = () => ({
+  type: ACTION_TYPES.GET_HEROES_REQUEST,
+});
+
+export const getHeroesSuccess = values => ({
+  type: ACTION_TYPES.GET_HEROES_SUCCESS,
+  values,
+});
+
+export const getHeroesError = error => ({
+  type: ACTION_TYPES.GET_HEROES_ERROR,
+  error,
+});
